5f4d12a7a7ffdb302c7c6021495df20b94126af7
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2023-01-23 13:20:16 -0600

    finish second component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "../style";
 import { discount, robot } from "../assets";
+import GetStarted from './GetStarted';
 
 const Hero = () => {
   return (
-    <section id="home" className={`flex  flex-col ${styles.paddingY}`}>
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div
         className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
       >
@@ -15,14 +16,18 @@ const Hero = () => {
             <span className="text-white">1 Month</span> Account
           </p>
         </div>
-      </div>
-      <div className="flex flex-row justify-between items-center w-full">
+        <div className="flex flex-row justify-between items-center w-full">
         <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
           The Next <br className="sm:block hidden" />
           <span className="text-gradient">Generation</span><br/>
           Payment Methond
         </h1>
+        <div className="ss:flex hidden md:mr-4 mr-0">
+          <GetStarted/>
+        </div>
       </div>
+      </div>
+
     </section>
   );
 };
