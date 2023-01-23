2eb0afe43c819547c8c139e9ec6d03cb5180d54f
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2023-01-23 15:53:49 +0200

    change className items-center

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
             toggle ? "flex" : "hidden"
           } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
-          <ul className="list-none flex flex-col justify-end items-center flex-1">
+          <ul className="list-none flex flex-col justify-end items-start flex-1">
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
