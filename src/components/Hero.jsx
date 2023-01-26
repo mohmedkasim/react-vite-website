01206db5dcc3d09565fb4f8eaff694948dbdd927
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2023-01-26 11:44:22 -0600

    add animation to button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
             The Next <br className="sm:block hidden" />{" "}
             <span className="text-gradient">Generation</span>{" "}
           </h1>
-          <div className="ss:flex hidden md:mr-4 mr-0">
+          <div className="ss:flex hidden md:mr-4 mr-0 transition hover:scale-95">
             <GetStarted />
           </div>
         </div>
